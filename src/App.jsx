ec631fd5102dc3937e3d84abb8afac9ea0a7c310
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-03-22 10:50:14 +0500

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
